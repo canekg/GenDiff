a5114923e2ff240f0fca632c2361a91992be97c1
Alexandr <canekg@mail.ru>
2023-06-10 16:17:03 +0500

    add bin/gendiff.js(commander)

---
+++ bin/gendiff.js
@@ -0,0 +1,18 @@
+#!/usr/bin/env node
+import { Command } from 'commander';
+// eslint-disable-next-line import/extensions
+import genDiff from '../src/index.js';
+
+const program = new Command();
+
+program
+  .name('gendiff')
+  .description('Compares two configuration files and shows a difference.')
+  .version('1.1.1');
+
+program
+  .argument('<filepath1>')
+  .argument('<filepath2>')
+  .option('-f, --format <type>', 'output format')
+  .action((filepath1, filepath2) => console.log(genDiff(filepath1, filepath2)));
+program.parse();
