c70204c4a992ba49a9207bff3aaf229679546cdf
Alexandr <canekg@mail.ru>
2023-06-12 13:41:15 +0500

    modified:   __tests__/index.test.js

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,11 +1,20 @@
+import fs from 'fs';
+import { fileURLToPath } from 'url';
+import path, { dirname } from 'path';
 import genDiff from '../src/index.js';
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const getFixturePath = (filename) =>
+  path.join(__dirname, '..', '__fixtures__', filename);
+const readFile = (filename) =>
+  fs.readFileSync(getFixturePath(filename), 'utf-8');
 const file1 = './__fixtures__/file1.json';
 const file2 = './__fixtures__/file2.json';
+const correctResult = readFile('result.txt');
 
 test('genDiff', () => {
   const actual1 = genDiff(file1, file2);
-  expect(actual1).toStrictEqual(
-    '- follow: false\n  host: hexlet.io\n- proxy: 123.234.53.22\n- timeout: 50\n+ timeout: 20\n+ verbose: true',
-  );
+  expect(actual1).toStrictEqual(correctResult);
 });
