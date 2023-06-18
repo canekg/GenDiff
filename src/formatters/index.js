7b2d8fef17d66dd62e82b3b54ab72804d8a158fe
Alexandr <canekg@mail.ru>
2023-06-18 23:13:54 +0500

    add case json

--- src/formatters/index.js
+++ src/formatters/index.js
@@ -7,6 +7,8 @@ const getFormat = (data, formatName) => {
       return formatStylish(data);
     case 'plain':
       return formatPlain(data);
+    case 'json':
+      return JSON.stringify(data);
     default:
       throw new Error(`'Unknown format! ${formatName}'`);
   }
