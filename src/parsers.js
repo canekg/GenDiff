7990a39abd6e45f1a5210476b880ed967d3e8ea9
Alexandr <canekg@mail.ru>
2023-06-18 23:15:58 +0500

    raname file

---
+++ src/parsers.js
@@ -0,0 +1,17 @@
+import path from 'path';
+import yaml from 'js-yaml';
+
+const parse = (data, filePath) => {
+  const extension = path.extname(filePath);
+  switch (extension) {
+    case '.json':
+      return JSON.parse(data);
+    case '.yml':
+      return yaml.load(data);
+    case '.yaml':
+      return yaml.load(data);
+    default:
+      throw new Error(`'Unknown format! ${extension}'`);
+  }
+};
+export default parse;
