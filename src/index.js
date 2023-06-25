23339dc590e9bc6c1d36310bded65198bd234d94
Alexandr <canekg@mail.ru>
2023-06-26 01:50:25 +0500

    correct func readFile, function removal getFullPath to the module level

--- src/index.js
+++ src/index.js
@@ -4,17 +4,19 @@ import parsers from './parsers.js';
 import getFormat from './formatters/index.js';
 import getTree from './diffTree.js';
 
+const getFullPath = (filePath) => path.resolve(process.cwd(), filePath);
+
 const readFile = (filePath) => {
-  const fullPath = path.resolve(process.cwd(), filePath);
-  const readData = fs.readFileSync(fullPath, 'utf-8');
-  const object = parsers(readData, filePath);
+  const readData = fs.readFileSync(getFullPath(filePath), 'utf-8');
+  const formatType = path.extname(filePath).slice(1);
+  const object = parsers(formatType)(readData);
   return object;
 };
 
-const genDiff = (filePath1, filePath2, formatName = 'stylish') => {
+const genDiff = (filePath1, filePath2, formatType = 'stylish') => {
   const file1 = readFile(filePath1);
   const file2 = readFile(filePath2);
   const tree = getTree(file1, file2);
-  return getFormat(tree, formatName);
+  return getFormat(formatType)(tree);
 };
 export default genDiff;
