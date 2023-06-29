be9053b11fa2285478b0660436d0f385d1559cfd
Alexandr <canekg@mail.ru>
2023-06-29 10:02:17 +0500

    modified:   bin/gendiff.js

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -11,5 +11,5 @@ program
   .action((filepath1, filepath2) => {
     const option = program.opts();
     console.log(genDiff(filepath1, filepath2, option.format));
-  });
-program.parse();
+  })
+  .parse();
