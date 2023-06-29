a7a59994831b057687aa6b04c26272d022b0e4aa
Alexandr <canekg@mail.ru>
2023-06-29 10:04:44 +0500

    modified: getFormat

--- src/formatters/index.js
+++ src/formatters/index.js
@@ -1,12 +1,12 @@
 import formatStylish from './stylish.js';
 import formatPlain from './plain.js';
 
-const getFormat = (formatType) => {
+const getFormat = (tree, formatType) => {
   const formats = {
     stylish: (data) => formatStylish(data),
     plain: (data) => formatPlain(data),
     json: (data) => JSON.stringify(data),
   };
-  return formats[formatType] ?? `Unknown format! ${formatType}`;
+  return formats[formatType] !== undefined ? formats[formatType](tree) : `Unknown format! ${formatType}`;
 };
 export default getFormat;
