8cd76620f255d1869ef286fa9c6b0549a8d0a6d0
Alexandr <canekg@mail.ru>
2023-06-29 10:03:45 +0500

    modified: getTree

--- src/diffTree.js
+++ src/diffTree.js
@@ -1,41 +1,41 @@
 import _ from 'lodash';
 
-const getTree = (file1, file2) => {
-  const keys = _.sortBy(_.union(Object.keys(file1), Object.keys(file2)));
+const getTree = (fileData1, fileData2) => {
+  const keys = _.sortBy(_.union(Object.keys(fileData1), Object.keys(fileData2)));
   const result = keys.map((key) => {
-    if (_.isPlainObject(file1[key]) && _.isPlainObject(file2[key])) {
+    if (_.isPlainObject(fileData1[key]) && _.isPlainObject(fileData2[key])) {
       return {
         key,
-        children: getTree(file1[key], file2[key]),
+        children: getTree(fileData1[key], fileData2[key]),
         type: 'nested',
       };
     }
-    if (!_.has(file1, key)) {
+    if (!_.has(fileData1, key)) {
       return {
         key,
         type: 'added',
-        value: file2[key],
+        value: fileData2[key],
       };
     }
-    if (!_.has(file2, key)) {
+    if (!_.has(fileData2, key)) {
       return {
         key,
         type: 'deleted',
-        value: file1[key],
+        value: fileData1[key],
       };
     }
-    if (_.isEqual(file1[key], file2[key])) {
+    if (_.isEqual(fileData1[key], fileData2[key])) {
       return {
         key,
         type: 'unchanged',
-        value: file1[key],
+        value: fileData1[key],
       };
     }
     return {
       key,
       type: 'changed',
-      value1: file1[key],
-      value2: file2[key],
+      value1: fileData1[key],
+      value2: fileData2[key],
     };
   });
   return result;
