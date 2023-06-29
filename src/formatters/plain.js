a164e163259e312ff3295d54070391c699eae5fa
Alexandr <canekg@mail.ru>
2023-06-29 10:08:07 +0500

    modified: src/formatters/plain.js, src/formatters/stylish.js

--- src/formatters/plain.js
+++ src/formatters/plain.js
@@ -1,37 +1,36 @@
 import _ from 'lodash';
 
-const getValue = (value) => {
+const stringify = (value) => {
   if (_.isString(value)) {
     return `'${value}'`;
   }
   if (_.isObject(value)) {
     return '[complex value]';
   }
+  if (value === null) {
+    return value;
+  }
   return value;
 };
 
-const getFullPath = (key, currentPath) => {
-  if (currentPath !== '') {
-    return `${currentPath}.${key}`;
-  }
-  return `${key}`;
-};
+const getFullPath = (key, currentPath) => (!currentPath ? `${key}` : `${currentPath}.${key}`);
 
 const iter = (diff, path) => diff
-  .filter((node) => node.type !== 'unchanged')
   .map((node) => {
     const currentPath = getFullPath(node.key, path);
     switch (node.type) {
       case 'deleted':
-        return `Property '${currentPath}' was removed`;
+        return `Property '${currentPath}' was removed\n`;
+      case 'unchanged':
+        return null;
       case 'added':
-        return `Property '${currentPath}' was added with value: ${getValue(node.value)}`;
+        return `Property '${currentPath}' was added with value: ${stringify(node.value)}\n`;
       case 'changed': {
-        return `Property '${currentPath}' was updated. From ${getValue(node.value1)} to ${getValue(node.value2)}`;
+        return `Property '${currentPath}' was updated. From ${stringify(node.value1)} to ${stringify(node.value2)}\n`;
       }
       case 'nested': {
         const lines = iter(node.children, currentPath);
-        return `${lines.join('\n')}`;
+        return `${lines.join('')}`;
       }
       default:
         throw new Error(`Unknown type of node '${node.type}'.`);
@@ -39,6 +38,6 @@ const iter = (diff, path) => diff
   });
 const formatPlain = (tree) => {
   const result = iter(tree, '');
-  return `${result.join('\n')}`;
+  return `${result.join('').trim()}`;
 };
 export default formatPlain;
--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -1,47 +1,48 @@
 import _ from 'lodash';
-import { getSpacesForChanged, getSpacesForUnchanged } from './spaces.js';
+
+const getSpaceCount = (depth, spacesCount = 4) => ' '.repeat(depth * spacesCount - 2);
 
 const stringify = (data, depth) => {
   if (!_.isPlainObject(data)) {
     return String(data);
   }
   const lines = Object.entries(data).map(
-    ([key, value]) => `${getSpacesForUnchanged(depth + 1)}${key}: ${stringify(value, depth + 1)}`,
+    ([key, value]) => `${getSpaceCount(depth + 1)}  ${key}: ${stringify(value, depth + 1)}`,
   );
-  return `{\n${lines.join('\n')}\n${getSpacesForUnchanged(depth)}}`;
+  return `{\n${lines.join('\n')}\n${getSpaceCount(depth)}  }`;
 };
 
 const iter = (tree, depth = 1) => tree.map((node) => {
   switch (node.type) {
     case 'deleted':
-      return `${getSpacesForChanged(depth)}- ${node.key}: ${stringify(
+      return `${getSpaceCount(depth)}- ${node.key}: ${stringify(
         node.value,
         depth,
       )}`;
     case 'added':
-      return `${getSpacesForChanged(depth)}+ ${node.key}: ${stringify(
+      return `${getSpaceCount(depth)}+ ${node.key}: ${stringify(
         node.value,
         depth,
       )}`;
     case 'changed': {
-      return `${getSpacesForChanged(depth)}- ${node.key}: ${stringify(
+      return `${getSpaceCount(depth)}- ${node.key}: ${stringify(
         node.value1,
         depth,
-      )}\n${getSpacesForChanged(depth)}+ ${node.key}: ${stringify(
+      )}\n${getSpaceCount(depth)}+ ${node.key}: ${stringify(
         node.value2,
         depth,
       )}`;
     }
     case 'unchanged':
-      return `${getSpacesForUnchanged(depth)}${node.key}: ${stringify(
+      return `${getSpaceCount(depth)}  ${node.key}: ${stringify(
         node.value,
         depth,
       )}`;
     case 'nested': {
       const lines = iter(node.children, depth + 1);
-      return `${getSpacesForUnchanged(depth)}${node.key}: {\n${lines.join(
+      return `${getSpaceCount(depth)}  ${node.key}: {\n${lines.join(
         '\n',
-      )}\n${getSpacesForUnchanged(depth)}}`;
+      )}\n${getSpaceCount(depth)}  }`;
     }
     default:
       throw new Error(`Unknown type of node '${node.type}'.`);
